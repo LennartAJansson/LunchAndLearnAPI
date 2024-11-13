140d2f8f22e69558cbb9f68d7add2273c28de913
lennartajansson <lennart.a.jansson@outlook.com>
2024-11-13 13:43:26 +0100

    SignalR

--- Slask/ConvertCsv/TotalRecord.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-public sealed class TotalRecord
-{
-  public DateTime Date { get; set; }
-  public float Temperatur { get; set; }
-  public float Fuktighet { get; set; }
-  public float Tryck { get; set; }
-}
